fafc211fc44c89cb15008a8cc0c49fa41ab4cdb7
Robert  Williams <robertmaxwilliams@gmail.com>
2017-10-02 18:52:27 -0400

    added LEDs

--- receiver_pong/receiver_pong.ino
+++ receiver_pong/receiver_pong.ino
@@ -3,11 +3,16 @@
 const byte HC12RxdPin = 4;                  // Recieve Pin on HC12
 const byte HC12TxdPin = 5;                  // Transmit Pin on HC12
 const byte MAX = 10;
+
+const int SUCCESS_PIN = LED_BUILTIN;
+
 SoftwareSerial HC12(HC12TxdPin,HC12RxdPin); // Create Software Serial Port
 
 void setup() {
   Serial.begin(1200);
   HC12.begin(1200);
+  pinMode(2 , OUTPUT);
+
 }
 
 unsigned long time_sent = millis();
@@ -17,6 +22,7 @@ void loop() {
   }
   while (HC12.available() > 0){
     if (HC12.peek() == 'a'){
+      digitalWrite(SUCCESS_PIN, HIGH);
       Serial.println(millis() - time_sent);
       HC12.read();
       //Serial.println("***Recived ping***");
@@ -24,9 +30,12 @@ void loop() {
       time_sent = millis();
       //Serial.println("Send reply");
       } else{
-      //Serial.print("recieved garbage: ");
+      //Serial.pr      digitalWrite(SUCCESS_PIN, HIGH);
+int("recieved garbage: ");
       //Serial.println((char)HC12.read());
       HC12.read();
     }
   }
+  delay(200);
+  digitalWrite(SUCCESS_PIN, LOW);
 }
--- transmitter_ping/transmitter_ping.ino
+++ transmitter_ping/transmitter_ping.ino
@@ -3,32 +3,45 @@
 const byte HC12RxdPin = 4;                  // Recieve Pin on HC12
 const byte HC12TxdPin = 5;                  // Transmit Pin on HC12
 const byte MAX = 10;
+
+const int SUCCESS_PIN = 2;
+const int FAILURE_PIN = 3;
 SoftwareSerial HC12(HC12TxdPin,HC12RxdPin); // Create Software Serial Port
 
 void setup() {
   Serial.begin(1200);                       // Open serial port to computer
   HC12.begin(1200);                         // Open serial port to HC12
+  pinMode(SUCCESS_PIN, OUTPUT);
+  pinMode(FAILURE_PIN, OUTPUT);
 }
 
 void loop() {
-
+  int success = 0;
   //send a lowercase a to the other one
   //Serial.println("Sending byte out");
   HC12.write('a');
 
+
   unsigned long time_start = millis();
   while (HC12.available() <= 0){
     if (millis() - time_start > 2000){
-      //Serial.println("timeout");
+      digitalWrite(FAILURE_PIN, HIGH);
+      Serial.println("timeout");
       break; 
     }
   }
 
   while (HC12.available() > 0){
     if (HC12.read() == 'b'){
-      //Serial.println("***Recived reply***");
+      success = 1;
+      digitalWrite(SUCCESS_PIN, HIGH);
+      Serial.println("***Recived reply***");
     } else{
-      //Serial.println("recieved garbage...");
+      Serial.println("recieved garbage...");
     }
   }
+  delay(200);
+  digitalWrite(SUCCESS_PIN, LOW);
+  digitalWrite(FAILURE_PIN, LOW);
+    
 }
